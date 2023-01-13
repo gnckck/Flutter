a2040a979c4c9e7dea42bc78f5f3bf1522284fdc
Jeong Jina <101711121+gnckck@users.noreply.github.com>
2023-01-13 16:46:06 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+void main() {
+  runApp(const SampleApp());
+}
+
+class SampleApp extends StatelessWidget {
+  const SampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample App',
+      theme: ThemeData(
+        primarySwatch : Colors.blue,
+      ),
+      home: const SampleAppPage(),
+    );
+  }
+}
+
+class SampleAppPage extends StatefulWidget {
+  const SampleAppPage({super.key});
+
+  @override
+  State<SampleAppPage> createState() => _SampleAppPageState();
+}
+
+class _SampleAppPageState extends State<SampleAppPage> {
+
+  bool toggle = true;
+  void _toggle() {
+    setState(() {
+      toggle = !toggle;
+    });
+  }
+
+  Widget _getToggleChild() {
+    if (toggle) {
+      return const Text('Toggle One');
+    } else {
+      return ElevatedButton(
+        style : ElevatedButton.styleFrom(
+          padding: const EdgeInsets.all(50.0),
+        ),
+        onPressed: () {},
+        child: const Text('Toggle Two'),
+        );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample App'),
+      ),
+      body: Center(
+        child: _getToggleChild(),
+      ),
+      floatingActionButton : FloatingActionButton(
+        onPressed: _toggle,
+        tooltip: 'Update Text',
+        child: const Icon(Icons.update),
+        ),
+    );    
+}
+}
