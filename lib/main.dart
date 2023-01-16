32c8a117d9c24094956d011b309c7079b224b29e
Jeong Jina <101711121+gnckck@users.noreply.github.com>
2023-01-16 13:17:47 +0900

    리스트뷰 빌더
    앱 화면 크기 알아내기
    MediaQuery.of(context).size

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,123 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
 
 void main() {
-  runApp(const SampleApp());
+  runApp(const MyApp());
 }
 
-class SampleApp extends StatelessWidget {
-  const SampleApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sample App',
-      theme: ThemeData(
-        primarySwatch : Colors.blue,
-      ),
-      home: const SampleAppPage(),
+    return const MaterialApp(
+      home: ListViewPage(),
     );
   }
 }
 
-class SampleAppPage extends StatefulWidget {
-  const SampleAppPage({super.key});
+class ListViewPage extends StatefulWidget {
+  const ListViewPage({super.key});
 
   @override
-  State<SampleAppPage> createState() => _SampleAppPageState();
+  State<ListViewPage> createState() => _ListViewPageState();
 }
 
-class _SampleAppPageState extends State<SampleAppPage> {
+class _ListViewPageState extends State<ListViewPage> {
 
-  bool toggle = true;
-  void _toggle() {
-    setState(() {
-      toggle = !toggle;
-    });
-  }
+  var titleList = [
+    'Dentist',
+    'Pharamacist',
+    'School teacher',
+    'IT manager',
+    'Account',
+    'Lawyer',
+    'Hairdresser',
+    'Physician',
+    'Web developer',
+    'Medical Secretary'
+  ];
 
-  Widget _getToggleChild() {
-    if (toggle) {
-      return const Text('Toggle One');
-    } else {
-      return ElevatedButton(
-        style : ElevatedButton.styleFrom(
-          padding: const EdgeInsets.all(50.0),
-        ),
-        onPressed: () {},
-        child: const Text('Toggle Two'),
-        );
-    }
-  }
+  var imageList = [
+    'image/1.png',
+    'image/2.png',
+    'image/3.png',
+    'image/4.png',
+    'image/5.png',
+    'image/6.png',
+    'image/7.png',
+    'image/8.png',
+    'image/9.png',
+    'image/10.png'
+  ];
+
+  var description = [
+    '1. There are different types of careers you can pursue in your life. Which one will it be?',
+    '2. There are different types of careers you can pursue in your life. Which one will it be?',
+    '3. There are different types of careers you can pursue in your life. Which one will it be?',
+    '4. There are different types of careers you can pursue in your life. Which one will it be?',
+    '5. There are different types of careers you can pursue in your life. Which one will it be?',
+    '6. There are different types of careers you can pursue in your life. Which one will it be?',
+    '7. There are different types of careers you can pursue in your life. Which one will it be?',
+    '8. There are different types of careers you can pursue in your life. Which one will it be?',
+    '9. There are different types of careers you can pursue in your life. Which one will it be?',
+    '10. There are different types of careers you can pursue in your life. Which one will it be?'
+  ];
+
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width * 0.6;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sample App'),
-      ),
-      body: Center(
-        child: _getToggleChild(),
-      ),
-      floatingActionButton : FloatingActionButton(
-        onPressed: _toggle,
-        tooltip: 'Update Text',
-        child: const Icon(Icons.update),
+        title: const Text(
+          'List View',
+          style: TextStyle(color: Colors.grey),
         ),
-    );    
+        backgroundColor: Colors.white,
+        elevation:10,
+      ),
+      body : ListView.builder(
+        itemCount: titleList.length,
+        itemBuilder: (context, index){
+          return Card(
+            child: Row(
+              children: [
+                SizedBox(
+                width: 100,
+                height : 100,
+                child: Image.asset(imageList[index]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Text(titleList[index],
+                      style: const TextStyle(
+                        fontSize:22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        width: width,
+                        child: Text(
+                          description[index],
+                          style: TextStyle(
+                           fontSize: 15,
+                           color:Colors.grey[500] 
+                          ))
+                      )
+                      
+                    ],))
+              ],
+              )
+          );
+        }
+        )
+    );
+  }
 }
-}
+
