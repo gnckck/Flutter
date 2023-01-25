5acda87c68eb35c888f06ab53786703b308b3d05
Jeong Jina <101711121+gnckck@users.noreply.github.com>
2023-01-25 15:10:30 +0900

    Bloc 상태관리
    더하기 예제

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/app.dart';
+import 'package:flutter_application_1/src/app.dart';
+import 'package:get/get.dart';
 
 
-void main() => runApp(const App());
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: App(),
+    );
+  }
+}
